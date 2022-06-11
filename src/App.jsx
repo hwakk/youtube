d87a9a2514bd50b3be2c5989836ba464b35a59c7
hwakk <x_xhwakk@naver.com>
2022-06-11 18:56:52 +0900

    feat: Youtube 클론 프로젝트 개발

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import VideoList from "./components/VideoList/VideoList";
+
+function App() {
+  const [videos, setVideos] = useState([]);
+  useEffect(() => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=25&key=AIzaSyD_B0mvR7qBDqq4gapewEttRWSYv6u-_64",
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideos(result.items))
+      .catch((error) => console.log("error", error));
+  }, []);
+  return <VideoList videos={videos} />;
+}
+
+export default App;
